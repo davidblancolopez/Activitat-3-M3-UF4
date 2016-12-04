c487428764f06562c7b7af3f2fd7c314d648e600
davidblancolopez <daviddbl94@gmail.com>
2016-12-04 13:28:43 +0100

    Cambios.

--- src/vista/aplicatiu/vistaAplicatiu.java
+++ src/vista/aplicatiu/vistaAplicatiu.java
@@ -1,7 +1,39 @@
 
 package vista.aplicatiu;
 
+import java.util.Scanner;
+
 
 public class vistaAplicatiu {
+   Scanner lector = new Scanner(System.in);
     
+    public int mostrarMEnu(){
+        int opcio;
+        
+        System.out.println("MENÚ: \n"
+                + "1. Crear figura.\n"
+                + "2. Veure les figures.\n"
+                + "3. Modificar figura.\n");
+        
+        do{
+        System.out.println("Quina opció vols?");
+            if (lector.hasNext()) {
+                opcio = 0;
+            }
+        opcio = lector.nextInt();
+        }while(comprovarMenu1(opcio));
+        
+        return 0;
+    }
+    
+    
+    public boolean comprovarMenu1(int opcio){
+        boolean comprovar = true;
+        
+        if (opcio < 0 && opcio > 3) {
+            comprovar = false;
+        }
+        
+        return comprovar;
+    }
 }
